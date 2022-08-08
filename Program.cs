85b49a029b94805761a7cb05baef8d65bdf43179
William Maitre <william.maitre@gmail.com>
2022-08-08 16:23:50 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PraticeLinq
+{
+    public class program
+    {
+        static void Main (string[] args)
+        {
+            var myVideoList = new List<string>() { "Zelda", "Mario Cart", "Ghost Recon", "Final Fantasy Fourteen" };
+
+            var gameName = myVideoList.OrderBy(myVideoList => myVideoList);
+
+            myVideoList.Sort();
+
+            foreach(var myVideo in myVideoList)
+            {
+                Console.WriteLine(myVideo);
+            }
+
+            //Console.WriteLine(gameName);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
